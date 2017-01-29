4fdfef9b8603cb88c46257ed7548c542cc4984c3
feelmyears <philipmeyers2017@u.northwestern.edu>
2017-01-29 10:34:39 -0600

    Cleaning

--- goldenglobes.py
+++ goldenglobes.py
@@ -2,6 +2,10 @@ from TweetDB import Tweet, TweetDB
 from kb import MOTION_PICTURE_AWARDS, TELEVISION_AWARDS
 import re
 from collections import Counter
+from nltk.corpus import stopwords
+from textblob import TextBlob
+from textblob.np_extractors import ConllExtractor
+import nltk
 
 USE_FULL_SET = True
 
@@ -11,8 +15,9 @@ class GoldenGlobes():
         self.tweetDB = tweets
 
     def show_awards(self):
-        for award in award:
+        for award in self.awards:
             print awards
+
     def find_host(self):
         name_pattern = ur'([A-Z][a-z]+(?: [A-Z][a-z]+)+)'
         p = re.compile(name_pattern)
@@ -24,24 +29,12 @@ class GoldenGlobes():
                 for h in matches:
                     host_counts[h] += 1
 
+        print host_counts.most_common()
         ml_host = host_counts.most_common(1)[0][0]
         return ml_host
 
     def find_awards(self):
-        winners=[]
-        name_pattern = ur'((@|#)[A-Z][a-z]+(?: [A-Z][a-z]+)+)'
-        p = re.compile(name_pattern)
-        for award in self.awards:
-            award_counter=Counter()
-            for t in self.tweetDB.tweets:
-                text=t.text
-                if award in text:
-                    matches = re.findall(p,text)
-                    for h in matches:
-                        award_counter[h]+=1
-            if (award_counter.most_common(1)!=None):
-                winners.append((award,award_counter.most_common(1)[0][0]))
-        return winners
+        pass
 
 
 
@@ -55,9 +48,13 @@ tweets.process_tweets()
 awards = MOTION_PICTURE_AWARDS + TELEVISION_AWARDS
 
 # Creating GoldenGlobes app
-
 gg = GoldenGlobes(awards, tweets)
-print "finding host"
-print gg.find_host()
-for winner in gg.find_awards():
-    print winner
+extractor = ConllExtractor()
+for t in gg.tweetDB.tweets:
+    blob = TextBlob(t.text, np_extractor=extractor)
+    print blob.noun_phrases
+
+# print "finding host"
+# print gg.find_host()
+# for winner in gg.find_awards():
+#     print winner
