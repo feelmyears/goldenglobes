d42bd35f57cc5ab4604099cb8eeaa9d0e78ae1fb
feelmyears <philipmeyers2017@u.northwestern.edu>
2017-02-05 14:42:17 -0600

    Created AwardCeremonyKB abstract class

--- kb.py
+++ kb.py
@@ -30,39 +30,51 @@ TELEVISION_AWARDS = [
     'Best Supporting Actress in a Series, Limited Series or Motion Picture made for Television'
 ]
 
-STOPWORDS = [
-    'best',
-    '-'
-]
 
-PERSON = 'PERSON'
-PRODUCTION = 'PRODUCTION'
-class KB():
-    AWARDS = [
-        ('Best Motion Picture - Drama', PRODUCTION),
-        ('Best Motion Picture - Musical or Comedy', PRODUCTION),
-        ('Best Director', PERSON),
-        ('Best Actor - Motion Picture Drama', PERSON),
-        ('Best Actor - Motion Picture Musical or Comedy', PERSON),
-        ('Best Actress - Motion Picture Drama', PERSON),
-        ('Best Actress - Motion Picture Musical or Comedy', PERSON),
-        ('Best Supporting Actor - Motion Picture', PERSON),
-        ('Best Supporting Actress - Motion Picture', PERSON),
-        ('Best Screenplay', PRODUCTION),
-        ('Best Original Score', PRODUCTION),
-        ('Best Original Song', PRODUCTION),
-        ('Best Foreign Language Film', PRODUCTION),
-        ('Best Animated Feature Film', PRODUCTION),
-        ('Cecil B. DeMille Award for Lifetime Achievement in Motion Pictures', PERSON),
-        ('Best Drama Series', PRODUCTION),
-        ('Best Comedy Series', PRODUCTION),
-        ('Best Actor in a Television Drama Series', PERSON),
-        ('Best Actor in a Television Comedy Series', PERSON),
-        ('Best Actress in a Television Drama Series', PERSON),
-        ('Best Actress in a Television Comedy Series', PERSON),
-        ('Best Limited Series or Motion Picture made for Television', PRODUCTION),
-        ('Best Actor in a Limited Series or Motion Picture made for Television', PERSON),
-        ('Best Actress in a Limited Series or Motion Picture made for Television', PERSON),
-        ('Best Supporting Actor in a Series, Limited Series or Motion Picture made for Television', PERSON),
-        ('Best Supporting Actress in a Series, Limited Series or Motion Picture made for Television', PERSON)
-    ]
+class AwardCeremonyKB():
+    PERSON = 'PERSON'
+    PRODUCTION = 'PRODUCTION'
+    def get_awards(self):
+        pass
+
+    def get_stopwords(self):
+        pass
+
+class GoldenGlobesKB(AwardCeremonyKB):
+    def get_awards(self):
+        awards = [
+            ('Best Motion Picture - Drama', AwardCeremonyKB.PRODUCTION),
+            ('Best Motion Picture - Musical or Comedy', AwardCeremonyKB.PRODUCTION),
+            ('Best Director', AwardCeremonyKB.PERSON),
+            ('Best Actor - Motion Picture Drama', AwardCeremonyKB.PERSON),
+            ('Best Actor - Motion Picture Musical or Comedy', AwardCeremonyKB.PERSON),
+            ('Best Actress - Motion Picture Drama', AwardCeremonyKB.PERSON),
+            ('Best Actress - Motion Picture Musical or Comedy', AwardCeremonyKB.PERSON),
+            ('Best Supporting Actor - Motion Picture', AwardCeremonyKB.PERSON),
+            ('Best Supporting Actress - Motion Picture', AwardCeremonyKB.PERSON),
+            ('Best Screenplay', AwardCeremonyKB.PRODUCTION),
+            ('Best Original Score', AwardCeremonyKB.PRODUCTION),
+            ('Best Original Song', AwardCeremonyKB.PRODUCTION),
+            ('Best Foreign Language Film', AwardCeremonyKB.PRODUCTION),
+            ('Best Animated Feature Film', AwardCeremonyKB.PRODUCTION),
+            ('Cecil B. DeMille Award for Lifetime Achievement in Motion Pictures', AwardCeremonyKB.PERSON),
+            ('Best Drama Series', AwardCeremonyKB.PRODUCTION),
+            ('Best Comedy Series', AwardCeremonyKB.PRODUCTION),
+            ('Best Actor in a Television Drama Series', AwardCeremonyKB.PERSON),
+            ('Best Actor in a Television Comedy Series', AwardCeremonyKB.PERSON),
+            ('Best Actress in a Television Drama Series', AwardCeremonyKB.PERSON),
+            ('Best Actress in a Television Comedy Series', AwardCeremonyKB.PERSON),
+            ('Best Limited Series or Motion Picture made for Television', AwardCeremonyKB.PRODUCTION),
+            ('Best Actor in a Limited Series or Motion Picture made for Television', AwardCeremonyKB.PERSON),
+            ('Best Actress in a Limited Series or Motion Picture made for Television', AwardCeremonyKB.PERSON),
+            ('Best Supporting Actor in a Series, Limited Series or Motion Picture made for Television', AwardCeremonyKB.PERSON),
+            ('Best Supporting Actress in a Series, Limited Series or Motion Picture made for Television', AwardCeremonyKB.PERSON)
+        ]
+        return awards
+
+    def get_stopwords(self):
+        stopwords = [
+            'best',
+            '-'
+        ]
+        return stopwords
